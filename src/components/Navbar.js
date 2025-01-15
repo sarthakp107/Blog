47938518f3f857c103d6a1e9075861fceb4a3a2e
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-16 02:02:24 +1100

    tailwind fun

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  return (
-    <div className=
-    'block  bg-gray-800 text-white  p-4'>
-        <div className="container flex flex-wrap items-center">
-        Sarthaks Blog
-            <ul>
-                <li>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
+    return (
+        <div className='bg-dark text-light p-2 text-lg '>
+            <ul className='flex justify-around'>
+                <li className=''>
+                    <Link to="/">Sarthaks Blog</Link>
                 </li>
-            
+<div>
+    
+</div>
+                <li>
+                    <Link to="/login" className='mr-2'>Login</Link>
+                    <Link to="/signup" className='mr-20'>Signup</Link>
+                    <Link to="/subscribe" className=' bg-gray-400 rounded-full py-1.5 px-3 hover:bg-gray-50'>Subscribe</Link>
+                </li>
+                
+
             </ul>
-        
         </div>
-    </div>
-  )
+    )
 }
