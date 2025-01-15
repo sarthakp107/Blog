225d76742e2df1393d5a2529f8db3d4837d539d9
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-16 02:18:57 +1100

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,60 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return (
-        <div className='bg-dark text-light p-2 text-lg '>
-            <ul className='flex justify-around'>
-                <li className=''>
-                    <Link to="/">Sarthaks Blog</Link>
-                </li>
-<div>
-    
-</div>
-                <li>
-                    <Link to="/login" className='mr-2'>Login</Link>
-                    <Link to="/signup" className='mr-20'>Signup</Link>
-                    <Link to="/subscribe" className=' bg-gray-400 rounded-full py-1.5 px-3 hover:bg-gray-50'>Subscribe</Link>
-                </li>
-                
+  return (
+    <nav className="flex justify-between items-center py-4 px-8 bg-gray-100 ">
+      {/* Logo Section */}
+      <div>
+        <Link to="/" className="text-2xl font-bold text-gray-800 hover:text-gray-600">
+          BLOG
+        </Link>
+      </div>
 
-            </ul>
-        </div>
-    )
+      {/* Navigation Links */}
+      <div>
+        <ul className="flex gap-8 text-lg text-gray-700">
+          <li>
+            <Link to="/home" className="hover:text-gray-900">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-gray-900">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/author" className="hover:text-gray-900">
+              Author
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Action Buttons */}
+      <div>
+        <ul className="flex items-center gap-6">
+          <li>
+            <Link to="/signup" className="text-gray-700 hover:text-gray-900">
+              Signup
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="text-gray-700 hover:text-gray-900">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/subscribe"
+              className="bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600 transition"
+            >
+              Subscribe
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
